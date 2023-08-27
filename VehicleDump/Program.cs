e15c08af171c506b158d9162aaecd94bce554603
gir489returns <gir489returns@outlook.com>
2023-08-26 23:26:03 -0400

    Refactored program to use the JSON from the github it came from dynamically, and use its Extras array to determine if the vehicle has Extras.

--- VehicleDump/Program.cs
+++ VehicleDump/Program.cs
@@ -1,28 +1,15 @@
-﻿using System.Xml;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
-foreach (string filename in Directory.GetFiles("..\\..\\..\\..\\Vehicles", "vehicles.meta")) //Only use the one from Update.rpf, the DLC ones say they use FLAG_EXTRAS_STRONG but don't actually use any Extras.
+HttpClient client = new HttpClient();
+string vehicleJson = await client.GetStringAsync("https://raw.githubusercontent.com/DurtyFree/gta-v-data-dumps/master/vehicles.json");
+
+dynamic data = JsonConvert.DeserializeObject(vehicleJson);
+
+foreach (var vehicle in data)
 {
-    XmlDocument doc = new XmlDocument();
-    doc.Load(filename);
-    XmlNodeList nodes = doc.DocumentElement.SelectNodes("/CVehicleModelInfo__InitDataList/InitDatas/Item");
-
-    var json = File.ReadAllText("..\\..\\..\\..\\Vehicles\\vehicles.json"); //https://raw.githubusercontent.com/DurtyFree/gta-v-data-dumps/master/vehicles.json
-    dynamic data = JsonConvert.DeserializeObject(json);
-
-    foreach (XmlNode node in nodes)
+    if ( vehicle.Extras.Count > 0)
     {
-        if (node.SelectSingleNode("flags").InnerText.Contains("EXTRA"))
-        {
-            var modelName = node.SelectSingleNode("modelName").InnerText.ToUpper();
-            foreach (var vehicle in data)
-            {
-                string vehicle_Name = vehicle.Name;
-                if (vehicle_Name.ToUpper() == modelName)
-                {
-                    Console.WriteLine(modelName + " //" + vehicle.DisplayName.English);
-                }
-            }
-        }
+        string vehicle_Name = vehicle.Name;
+        Console.WriteLine(vehicle_Name.ToUpper() + " //" + vehicle.DisplayName.English);
     }
 }
